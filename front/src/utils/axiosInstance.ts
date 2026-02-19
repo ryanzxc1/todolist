c8e4cdf790f3f53f802aa1c0fef1cb98baa6a7b4
ryanzxc1 <ryan17126@naver.com>
2026-02-19 17:12:48 +0900

    fix: api problem

--- front/src/utils/axiosInstance.ts
+++ front/src/utils/axiosInstance.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // 배포 시엔 환경 변수를 쓰고, 로컬 개발 시엔 localhost를 사용
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1',
+  // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1',
+  baseURL: 'http://13.124.238.31:5000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
